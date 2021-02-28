c6be1cf5d527afa0835fea144ec387810ce03209
EMarshall <emilyjmarshall121@gmail.com>
2021-02-28 03:26:39 -0700

    added automated ID

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-// Required dependencies
 const express = require('express');
-const notes = require('./Develop/data/db.json');
-
-// Instantiates the server
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+const notes = require('./develop/data/db.json');
 
 // Functions
-function findById(id, notesArray) {
-  const result = notesArray.filter(note => note.id === id)[0];
-  return result;
-  }
-
-// HTML Routes
-app.get('/', function (req, res) {
-  res.send('hello!');
-});
+function createNewNote(body, notesArray) {
+  console.log(body);
+  // our function's main code will go here!
+  // return finished code to post route for response
+  return body;
+}
 
 // API Routes
 app.get('/api/notes', (req, res) => {
   res.json(notes);
-  });
+});
 
-// Method to make the server listen
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+// POST Routes
+app.post('/api/notes', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = notes.length.toString();
+  
+  res.json(req.body);
+});
+
+// Listener for server
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
   });
